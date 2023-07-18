b01df47148eccc7a4068df12da3013022e35dd32
anwarkvzk <anvarvzk521@gmail.com>
2023-07-18 14:37:54 +0530

    Logout and clear cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,7 +41,16 @@ export const login = async (req, res, next) => {
       .status(200)
       .send(info);
   } catch (err) {
-    next(err)
+    next(err);
   }
 };
-export const logout = (req, res) => {};
+
+export const logout = async(req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been Logout");
+};
